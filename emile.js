f3eb31047edca5af8ee85be06e347570a7824404
Thomas Fuchs <thomas@fesch.at>
2009-11-13 19:34:54 +0100

    version that supports opacity on IE. needs cleanups.

--- emile.js
+++ emile.js
@@ -3,11 +3,21 @@
 
 (function(emile, container){
   var parseEl = document.createElement('div'),
+    IE = !!window.attachEvent && !window.opera,
     props = ('backgroundColor borderBottomColor borderBottomWidth borderLeftColor borderLeftWidth '+
     'borderRightColor borderRightWidth borderSpacing borderTopColor borderTopWidth bottom color fontSize '+
     'fontWeight height left letterSpacing lineHeight marginBottom marginLeft marginRight marginTop maxHeight '+
     'maxWidth minHeight minWidth opacity outlineColor outlineOffset outlineWidth paddingBottom paddingLeft '+
-    'paddingRight paddingTop right textIndent top width wordSpacing zIndex').split(' ');
+    'paddingRight paddingTop right textIndent top width wordSpacing zIndex').split(' '),
+    handlers = { 
+      opacity: function(el,v){
+        if(IE){
+          var s = el.style, f = el.currentStyle.filter;
+          if(v==1){ f = f.replace(/alpha\([^\)]*\)/gi,'') ? s.filter = f : s.removeAttribute('filter'); return; }
+          if(!el.currentStyle.hasLayout) s.zoom = 1;
+          s.filter = f.replace(/alpha\([^\)]*\)/gi,'')+'alpha(opacity='+((v<0.001 ? 0 : v) * 100)+')';
+        } else s.opacity = v.toFixed(3);
+      }};
 
   function parse(value){
     var v = parseFloat(value), u = value.replace(/^[\d\.]+/,'');
@@ -39,16 +49,19 @@
   container[emile] = function(el, style, opts){
     el = typeof el == 'string' ? document.getElementById(el) : el;
     opts = opts || {};
-    var target = normalize(style), comp = el.currentStyle ? el.currentStyle : getComputedStyle(el, null),
+    var target = normalize(style), comp = el.currentStyle || getComputedStyle(el, null),
       prop, current = {}, start = +new Date, dur = opts.duration||200, finish = start+dur, interval,
       easing = opts.easing || function(pos){ return (-Math.cos(pos*Math.PI)/2) + 0.5; };
     for(prop in target) current[prop] = parse(comp[prop]);
     interval = setInterval(function(){
       var time = +new Date, pos = time>finish ? 1 : (time-start)/dur;
-      for(prop in target)
-        el.style[prop] = target[prop].unit == 'color' ?
-          color(current[prop].value,target[prop].value,easing(pos)) :
-          (current[prop].value+(target[prop].value-current[prop].value)*easing(pos)).toFixed(3) + target[prop].unit;
+      for(prop in target) 
+        if(prop in handlers) 
+          handlers[prop](el, (current[prop].value+(target[prop].value-current[prop].value)*easing(pos)));
+        else
+          el.style[prop] = target[prop].unit == 'color' ?
+            color(current[prop].value,target[prop].value,easing(pos)) :
+            (current[prop].value+(target[prop].value-current[prop].value)*easing(pos)).toFixed(3) + target[prop].unit;
       if(time>finish) { clearInterval(interval); opts.after && opts.after(); }
     },10);
   }
