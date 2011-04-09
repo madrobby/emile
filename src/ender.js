c32f193272a297c90eb833840f06ce04dcbdc2a1
Dustin Diaz <polvero@gmail.com>
2011-04-09 10:57:50 -0700

    ender changes

--- src/ender.js
+++ src/ender.js
@@ -1,6 +1,6 @@
 !function () {
   var e = emile.noConflict();
-  $.augment({
+  $.ender({
     animate: function (o, after) {
       for (var i = 0; i < this.elements.length; i++) {
         e(this.elements[i], o, after);
